cf6d3643a686fe859ef471e1264749defcf07d66
DilyOng <46112650+DilyOng@users.noreply.github.com>
2025-11-20 19:56:27 +0000

    Deploying to gh-pages from @ DilyOng/DilyOng.github.io@0449dac7883a546456cc3dff8e1e8f7c9f328907 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,17 +55,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
@@ -74,7 +63,12 @@ ninja.data = [{
             },},{id: "news-new-paper-on-arxiv-a-bayesian-perspective-on-evidence-for-evolving-dark-energy-presenting-a-direct-bayesian-model-comparison-using-nested-sampling-to-analyse-desi-dr2-data-arxiv-2511-10631",
           title: 'New paper on arXiv: A Bayesian Perspective on Evidence for Evolving Dark Energy...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-unimpeded",
+          title: 'unimpeded',
+          description: "A public nested sampling database for Bayesian cosmology",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/unimpeded/";
+            },},{
         id: 'social-arxiv',
         title: 'arXiv',
         section: 'Socials',
